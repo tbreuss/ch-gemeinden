5ba923f730437921f74ebd2c2bd9d339f635de91
tbreuss <thomasbreuss@gmx.ch>
2019-04-23 05:21:43 +0200

    Use app.css

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -12,17 +12,7 @@
     <!--[if gt IE 8]><!-->
     <link rel="stylesheet" href="https://unpkg.com/purecss@1.0.0/build/grids-responsive-min.css">
     <!--<![endif]-->
-    <style>
-        #main { max-width: 800px; margin-left: auto; margin-right: auto; }
-        .pure-table--entries { width: 100%; }
-        .pure-table--entries th { white-space: nowrap; }
-        .pure-table--entries td:nth-child(1) { width: 1%; text-align: center; }
-        .pure-table--entries td:nth-child(2) { width: 1%; text-align: right; padding-right: 8%; white-space: nowrap; }
-        .pure-table--entries td:nth-child(3) { width: 1%; text-align: center; }
-        .pure-table--entries td:nth-child(4) { width: 1%; text-align: right; padding-right: 10%; }
-        .pure-table--entries td:nth-child(5) { white-space: nowrap; }
-        .pure-table--entries th:hover { cursor:pointer; }
-    </style>
+    <link rel="stylesheet" href="<?= Yii::$app->request->baseUrl ?>/static/app.css">
 </head>
 <body>
 <?= $content ?>
