1ee4d43830318c3b2e6a438de16184ff56299c1c
tbreuss <thomasbreuss@gmx.ch>
2019-04-23 21:14:37 +0200

    Added simple layout

--- frontend/layouts/DefaultLayout.js
+++ frontend/layouts/DefaultLayout.js
@@ -2,15 +2,83 @@ import m from "mithril";
 
 const DefaultLayout = {
     view(vnode) {
-        return m("#layout.app", [
-            m("div#main", [
-                m("div.content", [
-                    m("div.pure-g", [
-                        m("div.pure-u-1", vnode.children)
+        return [
+            m(".container", [
+                m(".pure-g.header", [
+                    m(".pure-u-1-3", [
+                        m("a.pure-menu-heading[href='.']", "CH-Gemeinden")
+                    ]),
+                ])
+            ]),
+            m("#layout.app", [
+                m("div#main", [
+                    m("div.content", [
+                        m("div.pure-g", [
+                            m("div.pure-u-1", vnode.children)
+                        ])
                     ])
                 ])
-            ])
-        ]);
+            ]),
+            m("div.footer",
+                m("div.container",
+                    [
+                        m("div.pure-g",
+                            [
+                                m("div.pure-u-1.pure-u-md-3-5.footer-about",
+                                    [
+                                        m("h3.footer-about__heading",
+                                            "Über dieses Projekt"
+                                        ),
+                                        m("p.footer-about__text",
+                                            "Dieses Projekt ist ein Beispiel für eine Mithril.js Single Page Application (SPA), \
+                                                die an eine REST-API angebunden ist, basierend auf dem Yii2 Framework."
+                                        )
+                                    ]
+                                ),
+                                m("div.pure-u-1.pure-u-md-2-5.footer-tools",
+                                    [
+                                        m("h3.footer-tools__heading",
+                                            "Toolset"
+                                        ),
+                                        m("ul.footer-tools__list",
+                                            [
+                                                m("li",
+                                                    m("a[target='_blank'][href='https://mithril.js.org']",
+                                                        "Mithril.js"
+                                                    )
+                                                ),
+                                                m("li",
+                                                    m("a[target='_blank'][href='https://www.yiiframework.com']",
+                                                        "Yii2 Framework"
+                                                    )
+                                                ),
+                                                m("li",
+                                                    m("a[target='_blank'][href='http://purecss.io']",
+                                                        "Pure.css"
+                                                    )
+                                                )
+                                            ]
+                                        )
+                                    ]
+                                )
+                            ]
+                        ),
+                        m("hr.footer-ruler"),
+                        m("div.pure-g",
+                            m("div.pure-u-1.footer-copyright",
+                                [
+                                    "Ein kleines ",
+                                    m("a[target='_blank'][href='https://www.tebe.ch']",
+                                        "tebe.ch"
+                                    ),
+                                    " Projekt"
+                                ]
+                            )
+                        )
+                    ]
+                )
+            )
+        ];
     }
 };
 
--- views/layouts/main.php
+++ views/layouts/main.php
@@ -5,14 +5,15 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="robots" content="index, follow">
-    <link rel="stylesheet" href="https://unpkg.com/purecss@1.0.0/build/pure-min.css" integrity="sha384-nn4HPE8lTHyVtfCBi5yW9d20FjT8BJwUXyWZT9InLYax14RDjBj46LmSztkmNP9w" crossorigin="anonymous">
+    <link rel="stylesheet" href="<?= Yii::$app->request->baseUrl ?>/static/app.css">
+    <link rel="stylesheet" href="<?= Yii::$app->request->baseUrl ?>/static/css/pure-min.css">
     <!--[if lte IE 8]>
-    <link rel="stylesheet" href="https://unpkg.com/purecss@1.0.0/build/grids-responsive-old-ie-min.css">
+    <link rel="stylesheet" href="<?= Yii::$app->request->baseUrl ?>/static/css/grids-responsive-old-ie-min.css">
     <![endif]-->
     <!--[if gt IE 8]><!-->
-    <link rel="stylesheet" href="https://unpkg.com/purecss@1.0.0/build/grids-responsive-min.css">
+    <link rel="stylesheet" href="<?= Yii::$app->request->baseUrl ?>/static/css/grids-responsive-min.css">
     <!--<![endif]-->
-    <link rel="stylesheet" href="<?= Yii::$app->request->baseUrl ?>/static/app.css">
+    <link rel="stylesheet" href="<?= Yii::$app->request->baseUrl ?>/static/css/styles.css">
 </head>
 <body>
 <?= $content ?>
