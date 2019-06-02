fc20806a43aebcc235034296752b7daf2a441193
tbreuss <thomasbreuss@gmx.ch>
2019-06-02 09:33:41 +0200

    use production & development environment

--- web/index.php
+++ web/index.php
@@ -1,8 +1,7 @@
 <?php
 
-// comment out the following two lines when deployed to production
-defined('YII_DEBUG') or define('YII_DEBUG', true);
-defined('YII_ENV') or define('YII_ENV', 'dev');
+define('YII_DEBUG', getenv('YII_DEBUG') ?? false);
+define('YII_ENV', getenv('YII_ENV') ?? 'prod');
 
 require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
